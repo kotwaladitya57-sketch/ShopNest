6cde88014497bf8cb71f50cbbc30a44cf9207a5a
kotwaladitya57-sketch <kotwaladitya57@gmail.com>
2026-07-24 20:46:57 +0530

    Updated project

--- Backend/index.js
+++ Backend/index.js
@@ -40,7 +40,7 @@ app.use('/api/analytics', require('./routes/analyticsRoutes'))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-    app.get('*', (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
     });
 } else {
